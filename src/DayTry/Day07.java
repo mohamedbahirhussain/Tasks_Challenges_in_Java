41540d40248867c14466c3d41866007931139c73
Bahir Hussain <mbahirh624@gmail.com>
2025-02-21 19:22:11 +0300

    Update

--- src/DayTry/Day07.java
+++ src/DayTry/Day07.java
@@ -1,5 +1,19 @@
 package DayTry;
 
-public class Day07 {
+import java.util.Scanner;
 
+public class Day07 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String name  = scn.nextLine().toLowerCase().trim();
+        System.out.println(name);
+
+        if(name.length() > 2 && name.endsWith("at")){
+            System.out.println(name + " end with at");
+        }
+        else {
+            System.out.println(name + " not end with at");
+        }
+    }
 }
