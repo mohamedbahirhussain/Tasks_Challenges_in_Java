37a2c795f391d4cbc19fc051425596744838adb3
Bahir Hussain <mbahirh624@gmail.com>
2025-03-02 22:32:42 +0300

    Updated

---
+++ src/day0103.java
@@ -0,0 +1,9 @@
+public class day0103 {
+    static{
+        System.out.println("Static block executed");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Class.forName("day0103");
+    }
+}
